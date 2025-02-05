742e6543f3640f084f70965c892ca9620f2c4a4c
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-02-05 21:17:36 +0900

    feat: User 모델 필드 추가 및 수정 (name, is_host, first_name, last_name)
    non-editable 필드로 인한 오류 수정

--- users/models.py
+++ users/models.py
@@ -3,4 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # AbstractUser의 모든 걸 사용하지만 내가 조작할 수 있는 class가 있는 것
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+    name = models.CharField(max_length=150, default="")
+    is_host = models.BooleanField(default=False)  # 이전에 생성된 모든 사용자는 is_host 값을 default인 False로 받게 된다.
