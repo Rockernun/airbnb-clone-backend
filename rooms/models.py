797c2717bbf9e0e268cfaa952c384851938f5018
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-02-10 15:42:45 +0900

    feat: Room 모델에 타임스탬프 필드 추가 및 Amenity 필드 수정
    - Room 모델에 created_at, updated_at 필드 추가
    - Amenity 모델의 description 필드를 null=True, blank=True, default="" 로 수정
    - Amenity 모델의 verbose_name_plural을 "Amenities"로 변경

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from common.models import CommonModel
 
-class Room(models.Model):
+class Room(CommonModel):
 
     """ Room Model Definition """
 
@@ -10,6 +10,7 @@ class Room(models.Model):
         PRIVATE_ROOM = ("private_room", "Private Room")
         SHARED_ROOM = ("shared_room", "Shared Room")
 
+    name = models.CharField(max_length=180, default="")
     country = models.CharField(max_length=50, default="한국")
     city = models.CharField(max_length=80, default="서울")
     price = models.PositiveIntegerField(default=0)  # 기본값 0원
@@ -26,9 +27,18 @@ class Room(models.Model):
     owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
     amenities = models.ManyToManyField("rooms.Amenity")
 
+    def __str__(self) -> str:
+        return self.name
+
 class Amenity(CommonModel):
 
     """ Amenity Definition """
 
     name = models.CharField(max_length=150, default="기본 어메니티")  # 기본 어메니티 이름 설정
-    description = models.CharField(max_length=150, default="")  # 기본값 빈 문자열
+    description = models.CharField(max_length=150, null=True, blank=True, default="")  # 기본값 빈 문자열
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Amenities"
