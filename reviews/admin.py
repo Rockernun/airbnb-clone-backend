fd72f554cef1a755530a5135acde5d88637a496d
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-02-14 16:52:30 +0900

    feat(admin): Django Admin 필터 기능 추가 (ScoreFilter, WordFilter)
    - ScoreFilter: 리뷰 점수(rating)에 따라 필터링 (bad: 3 이하, good: 4 이상)
    - WordFilter: 특정 단어(good, great, awesome)를 기준으로 필터링
    
    추가적으로, parameter_name 뒤에 ,를 붙여서 튜플로 해석되는 바람에 오류 발생 -> 주의할 것

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,6 +1,44 @@
 from django.contrib import admin
 from .models import Review
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words!"
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, queryset):  # reviews -> queryset (일관성 유지)
+        print("Score Filter Value:", self.value())
+        score = self.value()  # 선택된 필터 값 가져오기
+        
+        if score == "bad":
+            return queryset.filter(rating__lte=3)
+        elif score == "good":
+            return queryset.filter(rating__gte=4)
+        return queryset  # None일 경우 전체 결과 반환
+
+class ScoreFilter(admin.SimpleListFilter):
+    title = "Filter by score!"
+    parameter_name="score"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("bad", "Bad"),
+            ("good", "Good"),
+        ]
+    
+    def queryset(self, request, reviews):
+        score = self.value()
+        if score == "bad":
+            return reviews.filter(rating__lte=3)
+        else:
+            return reviews.filter(rating__gte=4)
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     
@@ -10,5 +48,10 @@ class ReviewAdmin(admin.ModelAdmin):
     )
 
     list_filter = (
+        WordFilter,
+        ScoreFilter,
         "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
     )
