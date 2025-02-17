c92bc61c74783f72a7854b3a45dfbe38a20494f9
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-02-17 20:59:41 +0900

    refactor: 카테고리 API 클래스 기반 뷰로 리팩토링
    - 기존 함수 기반 뷰를 클래스 기반 뷰(APIView)로 변경
    - 카테고리 목록 조회(Categories) 및 상세 조회/수정/삭제(CategoryDetail) 구현
    - get_object 메서드를 활용하여 중복 코드 제거

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from .models import Category
 
 class CategorySerializer(serializers.Serializer):
 
@@ -10,7 +11,7 @@ class CategorySerializer(serializers.Serializer):
     def create(self, validated_data):
         return Category.objects.create(**validated_data)
 
-    def update(self, instance, validate_data):
+    def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.kind = validated_data.get('kind', instance.kind)
         instance.save()
