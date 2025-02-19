8c6ed54f4253fd21850c87ba828b6e7d3b7ea086
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-02-19 10:42:53 +0900

    feat: Room API 인증 기능 추가
    - Room 생성 시 인증된 사용자만 등록 가능하도록 변경

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -14,12 +14,13 @@ class AmenitySerializer(ModelSerializer):
 class RoomDetailSerializer(ModelSerializer):
 
     owner = TinyUserSerializer(read_only=True)
-    amenities = AmenitySerializer(many=True)
-    category = CategorySerializer()
+    amenities = AmenitySerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
 
     class Meta:
         model = Room
         fields = "__all__"
+
 
 class RoomListSerializer(ModelSerializer):
     class Meta:
