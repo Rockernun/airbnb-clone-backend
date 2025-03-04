8bd27e39339e08d8565156c5573dfd9f8d2a1637
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-03-04 16:15:50 +0900

    feat: User 로그인 및 로그아웃 API 구현
    - LogIn API 추가 (사용자 인증 및 로그인 처리)
    - LogOut API 추가 (인증된 사용자 로그아웃 처리)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.exceptions import ParseError, NotFound
@@ -77,3 +78,33 @@ class ChangePassword(APIView):
             return Response(status=status.HTTP_200_OK)
         else:
             raise ParseError
+
+
+class LogIn(APIView):
+    
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username or not password:
+            raise ParseError
+        
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+
+        if user:
+            login(request, user)
+            return Response({"ok" : "Welcome!"})
+        else:
+            return Response({"error" : "Wrong Password"})
+
+class LogOut(APIView):
+    permission_classes = [IsAuthenticated]  # 인증된 사용자만 로그아웃 가능
+
+    def post(self, request):
+        logout(request)  # Django의 logout 함수 호출
+        return Response({"ok": "Logged out successfully!"})
+
